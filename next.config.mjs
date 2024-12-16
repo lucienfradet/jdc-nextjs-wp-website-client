f459101552b4a9edd0be41572570122f38192d99
lucienfradet <lucien.fradet@hotmail.com>
2024-12-16 13:11:47 -0500

    changed config to serve nextjs app on path provided in .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Serve the app under /jardindeschefs
+};
 
 export default nextConfig;
