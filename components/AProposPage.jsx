47c39c5081675f0002cf543d358408011a5d2247
lucienfradet <lucien.fradet@hotmail.com>
2025-03-18 10:06:10 -0400

    added podcast section to about page

--- components/AProposPage.jsx
+++ components/AProposPage.jsx
@@ -126,8 +126,6 @@ export default function AProposPage({
 
         {/* Section 2 */}
         <section className={styles.section2}>
-          {/* SVG Background - This is a placeholder that will be replaced later */}
-          
           <div className={styles.circlesContainer}>
             <div 
               className={`${styles.circle} ${styles.circle1} ${activeCircle === 1 ? styles.flipped : ''}`}
@@ -244,15 +242,53 @@ export default function AProposPage({
                 className={styles.videoEmbed}
               ></iframe>
               
-              {/* SVG placeholders - Will be replaced later */}
               <div className={styles.videoBackgroundSVGs}>
                 <svg className={styles.videoSvg1} width="317" height="317" viewBox="0 0 317 317" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M184.204 5.56159C195.491 -3.66502 212.515 1.13486 217.321 14.8987L229.795 50.6279C232.486 58.3352 239.4 63.7847 247.522 64.6007L285.177 68.3835C299.682 69.8407 308.326 85.2726 301.992 98.4031L285.548 132.488C282.001 139.841 283.036 148.583 288.203 154.903L312.154 184.204C321.38 195.491 316.58 212.516 302.816 217.321L267.087 229.795C259.38 232.486 253.931 239.4 253.115 247.522L249.332 285.177C247.875 299.682 232.443 308.326 219.312 301.992L185.227 285.548C177.875 282.001 169.133 283.036 162.812 288.203L133.511 312.154C122.224 321.38 105.2 316.58 100.394 302.817L87.9202 267.087C85.2293 259.38 78.3156 253.931 70.1929 253.115L32.5384 249.332C18.0328 247.875 9.38886 232.443 15.7235 219.312L32.1673 185.227C35.7145 177.875 34.6791 169.133 29.5125 162.812L5.5615 133.511C-3.66511 122.224 1.13477 105.2 14.8987 100.394L50.6278 87.9203C58.3352 85.2294 63.7846 78.3157 64.6006 70.193L68.3834 32.5385C69.8406 18.0328 85.2726 9.38895 98.403 15.7236L132.488 32.1674C139.841 35.7146 148.583 34.6792 154.903 29.5126L184.204 5.56159Z" fill="#A22D22"/>
                 </svg>
-                <svg className={styles.videoSvg2} width="337" height="340" viewBox="0 0 337 340" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M82.6287 21.2835C90.943 6.0793 108.102 -2.042 125.131 1.16707L283.465 31.004C300.494 34.2131 313.521 48.0226 315.731 65.2101L336.282 225.015C338.492 242.202 329.384 258.858 313.721 266.272L168.088 335.199C152.425 342.613 133.77 339.097 121.879 326.491L11.322 209.286C-0.568718 196.681 -2.99016 177.852 5.3242 162.648L82.6287 21.2835Z" fill="#153C66"/>
+                <svg className={styles.videoSvg2} width="337" height="340" viewBox="0 0 337 340" fill="none"  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M82.6287 21.2835C90.943 6.0793 108.102 -2.042 125.131 1.16707L283.465 31.004C300.494 34.2131 313.521 48.0226 315.731 65.2101L336.282 225.015C338.492 242.202 329.384 258.858 313.721 266.272L168.088 335.199C152.425 342.613 133.77 339.097 121.879 326.491L11.322 209.286C-0.568718 196.681 -2.99016 177.852 5.3242 162.648L82.6287 21.2835Z" fill="#24311B"/>
                 </svg>
               </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Section 5 - Podcast */}
+        <section className={styles.section5}>
+          <div className={styles.podcastContainer}>
+            <div className={styles.podcastContent}>
+              <h2>{pageContent["section-5"]["title"]}</h2>
+              {renderContent(pageContent["section-5"]["texte"])}
+              
+              <a 
+                href={pageContent["section-5"]["podcast-url"]} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className={styles.podcastButton}
+              >
+                Écouter le podcast
+              </a>
+            </div>
+            
+            <div className={styles.podcastImageWrapper}>
+              <a 
+                href={pageContent["section-5"]["podcast-url"]} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className={styles.podcastImageLink}
+              >
+                <WPImage 
+                  image={pageContent["section-5"]["podcast-img"]} 
+                  className={styles.podcastImage}
+                />
+              </a>
+            </div>
+            
+            <div className={styles.decorElements}>
+              <div className={styles.decorCircle1}></div>
+              <div className={styles.decorCircle2}></div>
+              <div className={styles.decorCircle3}></div>
             </div>
           </div>
         </section>
