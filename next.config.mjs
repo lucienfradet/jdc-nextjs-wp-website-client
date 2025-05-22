08027f3ed32cb7c126b74c9bac350518912a12f5
lucienfradet <lucien.fradet@hotmail.com>
2025-05-22 10:35:12 -0400

    added health check

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Serve the app under /jardindeschefs
   images: {
